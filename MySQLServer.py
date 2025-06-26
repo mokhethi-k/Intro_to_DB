8f20576962dc4c967fc1bf72e8997b41c1671976
mokhethi-k <mokhethikabelo@gmail.com>
2025-06-26 15:04:37 +0200

    Lets build your database: Your gateway to data adventure

---
+++ MySQLServer.py
@@ -0,0 +1,16 @@
+from getpass import getpass
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host = "localhost",
+        user = input("Enter username: "),
+        password = input("Enter password: ")
+    ) as connection:
+        with connection.cursor() as mycursor:
+            create_database_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+            mycursor.execute(create_database_query)
+            print("Database 'alx_book_store' created successfully!")
+            
+except Error as e:
+    print(e)
